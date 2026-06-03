36826f0a58d95721b640df14867d4cbfba9f5e9a
reveritnow <reveritnow@gmail.com>
2026-06-03 18:15:39 +0530

    Wire homepage sections together

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
+import SiteHeader from '@/components/SiteHeader'
 import HeroSection from '@/components/HeroSection'
 import TrustStrip from '@/components/TrustStrip'
 import CategoryGrid from '@/components/CategoryGrid'
 import FeaturedProducts from '@/components/FeaturedProducts'
+import ZpsExplainer from '@/components/ZpsExplainer'
 import SellerCTA from '@/components/SellerCTA'
+import SiteFooter from '@/components/SiteFooter'
 
 export default function HomePage() {
   return (
     <main>
+      <SiteHeader />
       <HeroSection />
       <TrustStrip />
       <CategoryGrid />
       <FeaturedProducts />
+      <ZpsExplainer />
       <SellerCTA />
+      <SiteFooter />
     </main>
   )
 }
