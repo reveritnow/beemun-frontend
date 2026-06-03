0a2327e8997e87162828bcc0c26626d64430c7c0
reveritnow <reveritnow@gmail.com>
2026-06-03 17:15:22 +0530

    Add TrustStrip component

---
+++ src/components/TrustStrip.tsx
@@ -0,0 +1,10 @@
+export default function TrustStrip() {
+  return (
+    <section>
+      <p>Zero Plastic</p>
+      <p>Zero Synthetic</p>
+      <p>Full Disclosure</p>
+      <p>BEEMUN Reviewed</p>
+    </section>
+  )
+}
